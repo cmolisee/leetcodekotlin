443f0170f85f88dea6a88c058267f757f8772465
codymolisee <cody.molisee@creditone.com>
2023-08-29 09:20:16 -0700

    added pre approval for gradle build scan in preperation for ci.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,18 @@
 
 rootProject.name = "leetcodekotlin"
 include("app")
+
+plugins {
+    id("com.gradle.enterprise") version("3.14.1")
+}
+
+// pre-approve the Terms & Conditions for gradle build scan
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
